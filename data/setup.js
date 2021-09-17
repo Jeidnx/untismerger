eb7a0cea9db611736bd0700c1ca11c9ccec908b3
Jeidnx <oliver@tegeler.de>
2021-09-17 11:47:26 +0200

    Removed unescessary commment

--- data/setup.js
+++ data/setup.js
@@ -2,27 +2,7 @@ if (localStorage.getItem('jwt')) {
 	window.location.href = '/';
 }
 
-submitForm = document.getElementById('form');
-/* formElem.onsubmit = async (e) => {
-	e.preventDefault();
-
-	let response = await fetch('/setup', {
-		method: 'POST',
-		//@ts-ignore
-		body: new FormData(formElem)
-	});
-
-	let result = await response.text();
-	console.log(result);
-
-	if (response.status === 200) {
-		localStorage.setItem('jwt', result);
-		window.location.href = '/';
-	} else {
-		console.log(response);
-		document.getElementById('return1').innerHTML = result;
-	}
-}; */
+let submitForm = document.getElementById('form');
 
 submitForm.onsubmit = (e) => {
 	e.preventDefault();
