69786b041a25da48409ea614cfe6be8795e31802
Jeidnx <oliver@tegeler.de>
2021-09-27 23:03:03 +0200

    Bumped version

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,13 @@
 // @ts-nocheck
-const cacheVersion = '1.1';
+const cacheVersion = '1.2';
 const cacheName = 'untmerger_v' + cacheVersion;
 const toCache = [
 	'/',
 	'/data/timetable.css',
 	'/data/timetable.js',
 	'/data/manifest.webmanifest',
+	'/data/icons/icon_apple.png',
+	'/data/icons/icon.png'
 ];
 
 self.addEventListener('install', (event) => {
@@ -34,19 +36,14 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
 	if (event.request.method === 'POST') {
-		console.log('[Service Worker] Skipping POST request');
 		return;
 	}
 	
-	console.log("[Service Worker] fetching: ");
-	console.log(event.request.url);
 	event.respondWith(
 		caches.match(event.request).then((response) => {
 			if (response) {
-				console.log("[Service Worker] Cache hit");
 				return response;
 			}
-			console.log("[Service Worker] Cache miss");
 			return fetch(event.request);
 		})
 	)
