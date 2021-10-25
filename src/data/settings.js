1fab871c903d9bff4d55216d45f8ae82997596eb
Jeidnx <oliver@tegeler.de>
2021-10-25 16:38:26 +0200

    Added temporary settings page

---
+++ src/data/settings.js
@@ -0,0 +1,40 @@
+const broadcast = new BroadcastChannel('sw-channel');
+
+broadcast.onmessage = (event) => {
+	if (event.data.type == 'VERSION') {
+		serviceWorkerVersion.innerHTML = 'Version: ' + event.data.body;
+	}
+};
+let serviceWorkerVersion = document.getElementById('serviceWorkerVersion');
+broadcast.postMessage({
+	type: 'GET',
+	body: 'VERSION'
+});
+
+document
+	.getElementById('serviceWorkerClearCache')
+	.addEventListener('click', () => {
+		broadcast.postMessage({
+			type: 'POST',
+			body: 'CLEARCACHE'
+		});
+	});
+document
+	.getElementById('serviceWorkerUnregister')
+	.addEventListener('click', () => {
+		navigator.serviceWorker.getRegistration().then((registration) => {
+			registration.unregister();
+		});
+	});
+document
+	.getElementById('serviceWorkerReregister')
+	.addEventListener('click', () => {
+		navigator.serviceWorker.getRegistration().then((registration) => {
+			registration.unregister().then(() => {
+				setTimeout(() => {
+					console.log('Reregistered');
+					navigator.serviceWorker.register('/sw.js');
+				}, 2000);
+			});
+		});
+	});
