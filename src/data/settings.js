e248945c7239c486db5fec6879cbe09c2fa7fb18
jonas-w <sejonas@protonmail.com>
2021-10-27 12:36:48 +0200

    Added HTML 5 Colorpickers

--- src/data/settings.js
+++ src/data/settings.js
@@ -1,76 +1,83 @@
 const broadcast = new BroadcastChannel('sw-channel');
 
 broadcast.onmessage = (event) => {
-	if (event.data.type == 'VERSION') {
-		serviceWorkerVersion.innerHTML = 'Version: ' + event.data.body;
-	}
+    if (event.data.type == 'VERSION') {
+        serviceWorkerVersion.innerHTML = 'Version: ' + event.data.body;
+    }
 };
 
 let serviceWorkerVersion = document.getElementById('serviceWorkerVersion');
 broadcast.postMessage({
-	type: 'GET',
-	body: 'VERSION'
+    type: 'GET',
+    body: 'VERSION'
 });
 // @ts-ignore
 document.getElementById('jwtKeyInput').value = localStorage.getItem('jwt');
 document
-	.getElementById('serviceWorkerClearCache')
-	.addEventListener('click', () => {
-		broadcast.postMessage({
-			type: 'POST',
-			body: 'CLEARCACHE'
-		});
-	});
+    .getElementById('serviceWorkerClearCache')
+    .addEventListener('click', () => {
+        broadcast.postMessage({
+            type: 'POST',
+            body: 'CLEARCACHE'
+        });
+    });
 document
-	.getElementById('serviceWorkerReregister')
-	.addEventListener('click', () => {
-		navigator.serviceWorker.getRegistration().then((registration) => {
-			if (typeof registration == 'undefined') {
-				navigator.serviceWorker.register('/sw.js');
-				return;
-			}
-			registration.unregister().then(() => {
-				setTimeout(() => {
-					console.log('Reregistered');
-					navigator.serviceWorker.register('/sw.js');
-				}, 2000);
-			});
-		});
-	});
+    .getElementById('serviceWorkerReregister')
+    .addEventListener('click', () => {
+        navigator.serviceWorker.getRegistration().then((registration) => {
+            if (typeof registration == 'undefined') {
+                navigator.serviceWorker.register('/sw.js');
+                return;
+            }
+            registration.unregister().then(() => {
+                setTimeout(() => {
+                    console.log('Reregistered');
+                    navigator.serviceWorker.register('/sw.js');
+                }, 2000);
+            });
+        });
+    });
+
 function updatePage() {
-	broadcast.postMessage({
-		type: 'GET',
-		body: 'VERSION'
-	});
-	// @ts-ignore
-	document.getElementById('jwtKeyInput').value = localStorage.getItem('jwt');
+    broadcast.postMessage({
+        type: 'GET',
+        body: 'VERSION'
+    });
+    // @ts-ignore
+    document.getElementById('jwtKeyInput').value = localStorage.getItem('jwt');
 }
+
 function deleteLocalCache() {
-	localStorage.removeItem('timeTable');
+    localStorage.removeItem('timeTable');
 }
 
 document
-	.getElementById('serviceWorkerReloadCache')
-	.addEventListener('click', () => {
-		broadcast.postMessage({
-			type: 'POST',
-			body: 'RELOADCACHE'
-		});
-	});
-document.getElementById('colorPickerRefresh').addEventListener('click', () => {
-	document.getElementById('colorPickerData').innerHTML = localStorage
-		.getItem('colorEnum')
-		// @ts-ignore
-		.replaceAll(',', ',\n');
-});
-document.getElementById('colorPickerData').innerHTML = localStorage
-	.getItem('colorEnum')
-	// @ts-ignore
-	.replaceAll(',', ', \n');
-document.getElementById('colorPickerSubmit').addEventListener('click', () => {
-	localStorage.setItem(
-		'colorEnum',
-		// @ts-ignore
-		document.getElementById('colorPickerData').innerHTML.replaceAll('\n', '')
-	);
-});
+    .getElementById('serviceWorkerReloadCache')
+    .addEventListener('click', () => {
+        broadcast.postMessage({
+            type: 'POST',
+            body: 'RELOADCACHE'
+        });
+    });
+
+//Setup color picker
+
+const json = JSON.parse(localStorage.getItem("colorEnum"));
+function colorPickerInit(){
+    var colorPickerHTML = "<h3>Farben für deine Fächer Auswählen</h3><button id='colorPickerRefresh'>Aktualisieren</button><table><tr><th>Fach</th><th>Farbe</th></tr>";
+    for (var key in json) {
+        colorPickerHTML += `<tr><td>${key}</td><td><input type="color" id="${key}" value="${json[key]}"/></td></tr>`
+    }
+    colorPickerHTML += "</table><button id='colorPickerSubmit'>Absenden</button>"
+    document.getElementById("colorPicker").innerHTML = colorPickerHTML;
+}
+colorPickerInit();
+document.getElementById("colorPickerSubmit").addEventListener('click', () => {
+    for (var key in json) {
+        json[key]=document.getElementById(key).value;
+    }
+    localStorage.setItem('colorEnum',JSON.stringify(json))
+})
+document.getElementById("colorPickerRefresh").addEventListener('click',()=>{
+    colorPickerInit();
+})
