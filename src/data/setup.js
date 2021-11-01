5a88d5995b19685bf4b879f382014e3cbde66d5d
Oliver <55414029+Jeidnx@users.noreply.github.com>
2021-11-01 17:04:05 +0100

    Setup via QR Code (#27)
    * Removed CSP, will be added later in headers
    
    * Added QR Code capability.
    
    * Added ui for QR Code reader
    
    * Exception handlers & UI

--- src/data/setup.js
+++ src/data/setup.js
@@ -1,7 +1,6 @@
 if (localStorage.getItem('jwt')) {
 	window.location.href = '/';
 }
-
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js');
 }
@@ -11,9 +10,78 @@ let submitForm = document.getElementById('form');
 let qrButton = document.getElementById('qrButton');
 let manuelButton = document.getElementById('manuelButton');
 
-submitForm.onsubmit = (e) => {
-	e.preventDefault();
+let barcodeDetector;
+if ('BarcodeDetector' in window) {
+	// @ts-ignore
+	qrButton.disabled = false;
+	// @ts-ignore
+	barcodeDetector = new BarcodeDetector({ formats: ['qr_code'] });
+}
+const videoSettings = {
+	video: {
+		width: {
+			min: 270,
+			ideal: 1080,
+			max: 1080
+		},
+		height: {
+			min: 270,
+			ideal: 1080,
+			max: 1080
+		},
+		aspectRatio: 1,
+		frameRate: { max: 30 },
+		facingMode: 'environment'
+	}
+};
+async function startVideo() {
+	return new Promise((resolve, reject) => {
+		navigator.mediaDevices
+			.getUserMedia(videoSettings)
+			.then((mStream) => {
+				resolve(mStream);
+				return;
+			})
+			.catch((err) => {
+				reject('Keine Kamera');
+			});
+	});
+}
+/**
+ * @param {ImageBitmap} bitmap
+ */
+async function detectQR(bitmap) {
+	return new Promise((resolve, reject) => {
+		if (typeof barcodeDetector === 'undefined') {
+			reject('BarcodeDetector API wird nicht unterstützt.');
+			return;
+		}
+		barcodeDetector
+			.detect(bitmap)
+			.then((qrArr) => {
+				if (qrArr.length > 0) {
+					const raw = qrArr[0].rawValue;
+					let obj = {};
+					raw
+						.split('?')[1]
+						.split('&')
+						.forEach((/** @type {string} */ element) => {
+							const e = element.split('=');
+							const key = e[0];
+							const val = e[1];
+							obj[key] = val;
+						});
 
+					resolve(obj);
+					return;
+				}
+				reject('Konnte kein QR Code finden');
+			})
+			.catch(console.log);
+	});
+}
+
+function submitHandler() {
 	let formBody = `stage=${stage}`;
 	//@ts-ignore
 	for (let i = 0; i < submitForm.elements.length; i++) {
@@ -65,6 +133,11 @@ submitForm.onsubmit = (e) => {
 	xhr.open('POST', '/setup');
 	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 	xhr.send(formBody);
+}
+
+submitForm.onsubmit = (e) => {
+	e.preventDefault();
+	submitHandler();
 };
 
 /**
@@ -73,3 +146,41 @@ submitForm.onsubmit = (e) => {
 function setStage(number) {
 	stage = number;
 }
+// QR / Video handler
+qrButton.addEventListener('click', async () => {
+	let videoStream = document.getElementById('videoStream');
+	let captureButton = document.getElementById('captureButton');
+	startVideo()
+		.then((mStream) => {
+			// @ts-ignore
+			videoStream.srcObject = mStream;
+			// @ts-ignore
+			let imageCp = new ImageCapture(mStream.getTracks()[0]);
+
+			captureButton.addEventListener('click', async () => {
+				imageCp.grabFrame().then((bitmap) => {
+					detectQR(bitmap)
+						.then((qrCode) => {
+							document.getElementById('qrStatus').innerHTML = 'Laden...';
+							// @ts-ignore
+							document.getElementById('usernameInput').value = qrCode.user;
+							// @ts-ignore
+							document.getElementById('secretInput').value = qrCode.key;
+							submitHandler();
+
+							mStream
+								.getTracks()
+								.forEach((/** @type {{ stop: () => void; }} */ track) => {
+									track.stop();
+								});
+						})
+						.catch(
+							(err) => (document.getElementById('qrStatus').innerHTML = err)
+						);
+				});
+			});
+		})
+		.catch((error) => {
+			document.getElementById('qrStatus').innerHTML = error;
+		});
+});
